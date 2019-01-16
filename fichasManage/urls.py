36571810fcbd15a2905be181192d70986403e7d2
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-01-16 10:38:41 -0500

    Added ubicaciones view template and url routing

--- fichasManage/urls.py
+++ fichasManage/urls.py
@@ -5,6 +5,7 @@ from fichasManage import views
 urlpatterns = [
     path(r'', views.index, name='index'),
     path(r'fichas/', views.fichas_campo_todas, name='fichas_campo_todas'),
+    path(r'ubicaciones/', views.ubicaciones_todas, name='ubicaciones_todas'),
     path(r'estructuras/', views.estructuras_geologicas_todas, name='estructuras_geologicas_todas'),
 ]
 
